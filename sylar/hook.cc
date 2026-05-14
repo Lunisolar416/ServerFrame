e4d4ecf7b24620a211e028d9dd707a3f1170e218
lunisolar416 <x1739930489@gmail.com>
2026-05-14 16:58:32 +0800

    +add XX(name)

--- sylar/hook.cc
+++ sylar/hook.cc
@@ -2,6 +2,7 @@
 #include "hook.h"
 #include "fiber.h"
 #include "iomanager.h"
+#include <ctime>
 #include <dlfcn.h>
 #include <sys/types.h>
 
@@ -12,7 +13,26 @@ static thread_local bool t_hook_enable = false;
 
 #define HOOK_FUN(XX)                                                                               \
     XX(sleep)                                                                                      \
-    XX(usleep)
+    XX(usleep)                                                                                     \
+    XX(nanosleep)                                                                                  \
+    XX(socket)                                                                                     \
+    XX(connect)                                                                                    \
+    XX(accept)                                                                                     \
+    XX(read)                                                                                       \
+    XX(readv)                                                                                      \
+    XX(recv)                                                                                       \
+    XX(recvfrom)                                                                                   \
+    XX(recvmsg)                                                                                    \
+    XX(write)                                                                                      \
+    XX(writev)                                                                                     \
+    XX(send)                                                                                       \
+    XX(sendto)                                                                                     \
+    XX(sendmsg)                                                                                    \
+    XX(close)                                                                                      \
+    XX(fcntl)                                                                                      \
+    XX(ioctl)                                                                                      \
+    XX(getsockopt)                                                                                 \
+    XX(setsockopt)
 
 void hook_init()
 {
