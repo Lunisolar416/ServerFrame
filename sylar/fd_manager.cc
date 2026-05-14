0b0d58c2d89d2a08e8b96f5550bfddc2c769334e
lunisolar416 <x1739930489@gmail.com>
2026-05-14 18:28:06 +0800

    fd funciton

---
+++ sylar/fd_manager.cc
@@ -0,0 +1,123 @@
+#include "fd_manager.h"
+#include "hook.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+namespace mysylar
+{
+FdCtx::FdCtx(int fd)
+    : m_isInit(false), m_isSocket(false), m_sysNonblock(false), m_userNonblock(false), m_fd(-1),
+      m_recvTimeout(-1), m_sendTimeout(-1)
+{
+}
+FdCtx::~FdCtx() {}
+
+bool FdCtx::init()
+{
+    if (m_isInit)
+    {
+        return true;
+    }
+    m_recvTimeout = -1;
+    m_sendTimeout = -1;
+
+    struct stat fd_stat;
+    // fstat 是 Linux/Unix
+    // 中用于获取“已打开文件描述符”的文件状态信息的系统调用。-1代表失败，成功返回0
+    if (-1 == fstat(m_fd, &fd_stat))
+    {
+        m_isInit = false;
+        m_isSocket = false;
+    }
+    else
+    {
+        m_isInit = true;
+        m_isSocket = S_ISSOCK(fd_stat.st_mode);
+    }
+
+    if (m_isSocket)
+    {
+        int flags = fcntl_f(m_fd, F_GETFL, 0);
+        if (!(flags & O_NONBLOCK))
+        {
+            fcntl_f(m_fd, F_SETFL, flags | O_NONBLOCK);
+        }
+        m_sysNonblock = true;
+    }
+    else
+    {
+        m_sysNonblock = false;
+    }
+    m_userNonblock = false;
+    m_isClosed = false;
+    return m_isInit;
+}
+
+void FdCtx::setTimeout(int type, uint64_t v)
+{
+    if (type == SO_RCVTIMEO)
+    {
+        m_recvTimeout = v;
+    }
+    else
+    {
+        m_sendTimeout = v;
+    }
+}
+uint64_t FdCtx::getTimeout(int type)
+{
+    if (type == SO_RCVTIMEO)
+    {
+        return m_recvTimeout;
+    }
+    else
+    {
+        return m_sendTimeout;
+    }
+}
+
+FdManager::FdManager()
+{
+    m_datas.resize(64);
+}
+FdCtx::ptr FdManager::get(int fd, bool auto_create)
+{
+    if (fd == -1)
+    {
+        return nullptr;
+    }
+    RwMutexType::ReadLock lock(m_mutex);
+    if (fd >= (int) m_datas.size())
+    {
+        if (auto_create == false)
+        {
+            return nullptr;
+        }
+    }
+    else
+    {
+        if (m_datas[fd] || !auto_create)
+        {
+            return m_datas[fd];
+        }
+    }
+    lock.unlock();
+    RwMutexType::WriteLock lock2(m_mutex);
+    FdCtx::ptr ctx(new FdCtx(fd));
+    if (fd >= (int) m_datas.size())
+    {
+        m_datas.resize(fd * 1.5);
+    }
+    m_datas[fd] = ctx;
+    return ctx;
+}
+void FdManager::del(int fd)
+{
+    RwMutexType::WriteLock lock(m_mutex);
+    if (fd >= (int) m_datas.size())
+    {
+        return;
+    }
+    m_datas[fd].reset();
+}
+
+} // namespace mysylar
---
+++ sylar/fd_manager.h
@@ -0,0 +1,93 @@
+#ifndef __SYLAR_FD_MANAGER_H__
+#define __SYLAR_FD_MANAGER_H__
+
+#include "mutex.h"
+#include "singleton.h"
+#include "thread.h"
+#include <memory>
+namespace mysylar
+{
+class FdCtx : public std::enable_shared_from_this<FdCtx>
+{
+  public:
+    typedef std::shared_ptr<FdCtx> ptr;
+    FdCtx(int fd);
+    ~FdCtx();
+
+    bool isInit() const
+    {
+        return m_isInit;
+    }
+    bool isSocket() const
+    {
+        return m_isSocket;
+    }
+    bool isClose() const
+    {
+        return m_isClosed;
+    }
+
+    void setUserNonblock(bool v)
+    {
+        m_userNonblock = v;
+    }
+    bool getUserNonblock() const
+    {
+        return m_userNonblock;
+    }
+
+    void setSysNonblock(bool v)
+    {
+        m_sysNonblock = v;
+    }
+    bool getSysNonblock() const
+    {
+        return m_sysNonblock;
+    }
+
+    void setTimeout(int type, uint64_t v);
+    uint64_t getTimeout(int type);
+
+  private:
+    bool init();
+
+  private:
+    bool m_isInit;
+    bool m_isSocket;
+    bool m_sysNonblock;
+    bool m_userNonblock;
+    bool m_isClosed;
+    int m_fd;
+    uint64_t m_recvTimeout;
+    uint64_t m_sendTimeout;
+};
+
+class FdManager
+{
+  public:
+    typedef RWMutex RwMutexType;
+    FdManager();
+
+    /**
+     * @brief 获取/创建文件句柄类FdCtx
+     *
+     * @param fd
+     * @param auto_create
+     * @return FdCtx::ptr
+     */
+    FdCtx::ptr get(int fd, bool auto_create = false);
+
+    void del(int fd);
+
+  private:
+    // 读写锁
+    RwMutexType m_mutex;
+    // 文件句柄合集
+    std::vector<FdCtx::ptr> m_datas;
+};
+
+typedef Singleton<FdManager> FdMgr;
+
+} // namespace mysylar
+
+#endif // __SYLAR_FD_MANAGER_H__
