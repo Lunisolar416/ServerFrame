6cebd97b687de82d47f06a6de7209a844508e53e
lunisolar416 <x1739930489@gmail.com>
2026-05-14 16:22:30 +0800

    add nanosleep and socket hook functions

--- build/CMakeFiles/logtest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for logtest.
-# This may be replaced when dependencies are built.
--- sylar/hook.h
+++ sylar/hook.h
@@ -5,6 +5,9 @@
 // 在Linux系统中，Hook技术常用于调试、性能分析、安全监控等
 
 // Socket相关函数的都是同步的，Hook技术可以将这些函数替换为异步的版本，从而实现非阻塞的网络编程。这对于高性能服务器开发非常有用，可以提高系统的吞吐量和响应速度。
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 namespace mysylar
 {
@@ -22,6 +25,19 @@ extern "C"
 
     typedef int (*usleep_fun)(useconds_t usec);
     extern usleep_fun usleep_f;
+
+    typedef int (*nanosleep_fun)(const struct timespec* req, struct timespec* rem);
+    extern nanosleep_fun nanosleep_f;
+
+    // socket
+    typedef int (*socket_fun)(int domain, int type, int protocol);
+    extern socket_fun socket_f;
+
+    typedef int (*connect_fun)(int sockfd, const struct sockaddr* addr, socklen_t addrlen);
+    extern connect_fun connect_f;
+
+    typedef int (*accept_fun)(int s, struct sockaddr* addr, socklen_t* addrlen);
+    extern accept_fun accept_f;
 }
 
 #endif //__SYLAR__HOOK_H__
