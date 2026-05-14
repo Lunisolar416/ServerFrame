9f4555d40b67b7ff179742ead9ca27c3489497b8
lunisolar416 <x1739930489@gmail.com>
2026-05-14 14:48:52 +0800

    test fiber func

--- test/test_fiber.cc
+++ test/test_fiber.cc
@@ -5,8 +5,9 @@ mysylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
 void run_in_fiber()
 {
     SYLAR_LOG_INFO(g_logger) << "run in fiber begin";
-    mysylar::Fiber::GetThis()->swapOut();
-    SYLAR_LOG_INFO(g_logger) << "run in fiber end";
+    mysylar::Fiber::GetThis()->back();
+    // 运行完这个就该返回到主线程里
+    SYLAR_LOG_INFO(g_logger) << "run in fiber end"; // 运行完这个，子协程就会结束
 }
 void test_fiber()
 {
@@ -20,6 +21,7 @@ void test_fiber()
 }
 int main()
 {
+    /*
     mysylar::Thread::setName("Main Thread");
     SYLAR_LOG_INFO(g_logger) << "main begin";
     std::vector<mysylar::Thread::ptr> thrs;
@@ -32,6 +34,18 @@ int main()
     {
         i->join();
     }
-    SYLAR_LOG_INFO(g_logger) << "main end";
+    SYLAR_LOG_INFO(g_logger) << "main end";*/
+    SYLAR_LOG_INFO(g_logger) << "test_fiber begin";
+    mysylar::Fiber::
+        GetThis(); // 初始，t_fiber
+                   // 不存在，getthis初始化了main_fiber,main_fiber只保存ctx,t_threadFiber
+                   // = main_fiber
+    mysylar::Fiber::ptr fiber(new mysylar::Fiber(run_in_fiber));
+
+    SYLAR_LOG_DEBUG(g_logger) << "create fiber";
+    fiber->call(); // 通过call函数里面的SetThis(),将t_fiber = fiber,然后就swap
+    SYLAR_LOG_INFO(g_logger) << "test_fiber after swapIn";
+    fiber->call();
+    SYLAR_LOG_INFO(g_logger) << "test_fiber after end";
     return 0;
 }
